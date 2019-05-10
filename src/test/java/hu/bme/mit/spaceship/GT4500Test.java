d1f8ab06ab1e5e321cbabf57738a233574aa37a1
unknown <acsbendi28@gmail.com>
2019-05-10 12:50:19 +0200

    Added method call verifications to unit test of GT4500.

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -30,6 +30,8 @@ public class GT4500Test {
 
     // Assert
     assertEquals(true, result);
+    verify(mockFirstTorpedoStore, times(1)).isEmpty();
+    verify(mockFirstTorpedoStore, times(1)).fire(1);
   }
 
   @Test
@@ -45,6 +47,10 @@ public class GT4500Test {
 
     // Assert
     assertEquals(true, result);
+    verify(mockFirstTorpedoStore, times(1)).isEmpty();
+    verify(mockFirstTorpedoStore, times(1)).fire(1);
+    verify(mockSecondTorpedoStore, times(1)).isEmpty();
+    verify(mockSecondTorpedoStore, times(1)).fire(1);
   }
 
 }
